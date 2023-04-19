281386db0a768e5496548426fc3ecf8dfa8d9ddd
jubran nassar <jubrann@spacelift.io>
2023-04-19 18:47:20 -0400

    removes example stack

--- main.tf
+++ main.tf
@@ -11,17 +11,6 @@ provider "spacelift" {
   # Configuration options
 }
 
-resource "spacelift_stack" "sandbox-child" {
-  branch     = "main"
-  name       = "example"
-  repository = "sandbox"
-  space_id   = "legacy-child-01GXBM7578FN4Z5XY17T30DNZV"
-  github_enterprise {
-    namespace = "jubranNassar"
-  }
-}
-
-
 resource "spacelift_stack" "mercedez-benz" {
   branch            = "main"
   name              = "mercedes-benz"

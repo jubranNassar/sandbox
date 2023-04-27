5bbd1eeba32013a4f149edb0e7dbb4165c6f07a2
jubran nassar <jubrann@spacelift.io>
2023-04-27 13:18:55 -0400

    creates sacks

---
+++ external-state-access/stack1/main.tf
@@ -0,0 +1,14 @@
+
+ 
+
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+output "hello_world" {
+  value = "hello world!"
+}
+
+output "hello_dog" {
+  value = "hello dog!"
+}
---
+++ external-state-access/stack2/main.tf
@@ -0,0 +1,21 @@
+
+terraform {
+  required_version = ">= 1.0.2"
+}
+
+data "terraform_remote_state" "stack1" {
+  backend = "remote"
+
+  config = {
+    hostname     = "spacelift.io"
+    organization = "msamendinger"
+
+    workspaces = {
+      name = "stack1"
+    }
+  }
+}
+
+output "hello_dog_stack1" {
+  value = data.terraform_remote_state.stack1.outputs.hello_dog
+}

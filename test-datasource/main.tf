f3693d7c1fbde2a1125f60981c995d768263cf6e
jubran nassar <jubrann@spacelift.io>
2023-11-15 12:58:35 -0500

    test

---
+++ test-datasource/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.6.0"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+data "spacelift_space_by_path" "test" {
+  space_path = "path/to/space"
+}

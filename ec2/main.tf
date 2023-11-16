b17208e80eff17a0cba1fc26f439f119082430fc
jubran nassar <jubrann@spacelift.io>
2023-11-16 13:55:15 -0500

    adds datasource

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,3 +5,22 @@ resource "aws_instance" "test-instance" {
       name = "test-instance"
     }
 }
+
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.6.0"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+data "spacelift_space_by_path" "test" {
+  space_path = "root/demo-resources"
+}
+
+output "this" {
+    value = data.spacelift_space_by_path.test.id
+}

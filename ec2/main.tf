348eaeedc243f2637a4f6989b7e7d892fc66d2c5
jubran nassar <jubrann@spacelift.io>
2023-11-16 16:48:28 -0500

    removes data source

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,17 +1,7 @@
 resource "aws_instance" "test-instance" {
-  instance_type = var.instance_type
-  ami           = data.aws_ami.dev_server_ami.id
-  tags = {
-    name = "test-instance"
-  }
-}
-
-provider "spacelift" {}
-
-data "spacelift_space_by_path" "test" {
-  space_path = "root/demo-resources"
-}
-
-output "this" {
-  value = data.spacelift_space_by_path.test.id
+    instance_type = var.instance_type
+    ami = data.aws_ami.dev_server_ami.id
+    tags = {
+      name = "test-instance"
+    }
 }
--- ec2/providers.tf
+++ ec2/providers.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.25.0"
-    }
-    spacelift = {
-      source  = "spacelift-io/spacelift"
-      version = "1.6.0"
+        source = "hashicorp/aws"
+        version = "5.25.0"
     }
   }
 }

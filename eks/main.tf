595b09e9a26c03cb59ea1e13dbfc262c14ac8322
jubran nassar <jubrann@spacelift.io>
2023-12-21 12:04:04 -0500

    adds variables

--- eks/main.tf
+++ eks/main.tf
@@ -1,4 +1,7 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.21.0"
+
+  subnet_ids               = var.subnet_ids
+  control_plane_subnet_ids = var.subnet_ids
 }
---
+++ eks/variables.tf
@@ -0,0 +1,3 @@
+variable "subnet_ids" {
+  default = ["subnet-0a93d9b3e8c6d4272"]
+}

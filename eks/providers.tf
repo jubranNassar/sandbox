4abe80c022ebd73ccf50055cf07c09bd706140b7
jubran nassar <jubrann@spacelift.io>
2023-12-21 11:54:12 -0500

    creates eks cluster

---
+++ eks/providers.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.57.0"
+    }
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.8.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">=2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.10"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "spacelift" {}

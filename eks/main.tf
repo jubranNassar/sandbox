6c27ec15e3290d2cce34e443224607ef29c83ce0
jubran nassar <jubrann@spacelift.io>
2023-12-21 16:40:07 -0500

    creates eks cluster

--- eks/main.tf
+++ eks/main.tf
@@ -2,10 +2,24 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.21.0"
 
-  cluster_name              = var.cluster_name
-  subnet_ids                = var.subnet_ids
-  control_plane_subnet_ids  = var.subnet_ids
+  cluster_name                   = var.cluster_name
+  subnet_ids                     = var.subnet_ids
+  control_plane_subnet_ids       = var.subnet_ids
+  cluster_endpoint_public_access = true
+
   manage_aws_auth_configmap = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
   eks_managed_node_groups = {
     initial = {

b2aa87267d91e703af546b1188abc6a8496ff314
jubran nassar <jubrann@spacelift.io>
2023-12-21 13:25:21 -0500

    updates provider

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,5 +1,5 @@
 variable "subnet_ids" {
-  default = ["subnet-03a84133c161f5a67","subnet-04d198ec1dc6ee9c5"]
+  default = ["subnet-03a84133c161f5a67", "subnet-04d198ec1dc6ee9c5"]
 }
 
 variable "cluster_name" {

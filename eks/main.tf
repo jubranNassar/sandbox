2e410b80de2ba7688a3f7268548b7c91e9b7cb98
jubran nassar <jubrann@spacelift.io>
2023-12-21 12:06:49 -0500

    adds cluster name

--- eks/main.tf
+++ eks/main.tf
@@ -2,6 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.21.0"
 
+  cluster_name             = var.cluster_name
   subnet_ids               = var.subnet_ids
   control_plane_subnet_ids = var.subnet_ids
 }
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,3 +1,7 @@
 variable "subnet_ids" {
   default = ["subnet-0a93d9b3e8c6d4272"]
 }
+
+variable "cluster_name" {
+  default = "dev-cluster"
+}

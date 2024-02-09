818968261707b563ddee35f2a25185a9000a9aed
jubran nassar <jubrann@spacelift.io>
2024-02-09 14:01:19 -0500

    test

---
+++ output-example/main.tf
@@ -0,0 +1,12 @@
+output "this" {
+    value = {
+  "private_health" = "https://sms-audit.api.io.qa.velocify.net/health"
+  "private_info" = "https://sms-audit.api.io.qa.velocify.net/info"
+  "private_root" = "https://sms-audit.api.io.qa.velocify.net"
+  "public_health" = ""
+  "public_info" = ""
+  "public_root" = ""
+  "waf_edge" = ""
+  "waf_origin" = ""
+}
+}

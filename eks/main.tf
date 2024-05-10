0fb2119c5bd17c8b37083589c5ccd0033138b246
jubran nassar <jubrann@spacelift.io>
2024-05-10 18:26:15 -0400

    adds roles

--- eks/main.tf
+++ eks/main.tf
@@ -13,6 +13,17 @@ module "eks" {
     {
       userarn  = "arn:aws:iam::657848498343:assumed-role/AWSReservedSSO_AWSAdministratorAccess_3f3b6cc7551cce44/jubrann@spacelift.io",
       uesrname = "jubran"
+    },
+    {
+      userarn = "arn:aws:iam::657848498343:role/AWSReservedSSO_AWSAdministratorAccess_3f3b6cc7551cce44",
+      userarn = "jubran2"
+    }
+  ]
+
+  aws_auth_roles = [
+    {
+      rolearn = "arn:aws:iam::657848498343:role/AWSReservedSSO_AWSAdministratorAccess_3f3b6cc7551cce44",
+      uesrname = "jubran"
     }
   ]
   

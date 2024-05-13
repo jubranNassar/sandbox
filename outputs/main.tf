fbaab6e995b840a0b7d617347f79f46800e8aba7
jubran nassar <jubrann@spacelift.io>
2024-05-13 14:42:22 -0400

    removes aws provider

--- outputs/main.tf
+++ outputs/main.tf
@@ -6,10 +6,6 @@ output "ips" {
 
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.28.0"
-    }
     spacelift = {
       source  = "spacelift-io/spacelift"
       version = "1.8.0"
